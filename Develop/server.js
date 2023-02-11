c562ab1f56fe09ff60855fdf922f8255ec9c4056
mar <rojanomariano@gmail.com>
2023-02-11 07:29:22 -0800

    require express and set up path to index.js

---
+++ Develop/server.js
@@ -0,0 +1,6 @@
+const express = require("express");
+// using this path to send to notes.html
+const path = require("path");
+// requiring the index.js file 
+const api = require("./routes/index.js")
+
