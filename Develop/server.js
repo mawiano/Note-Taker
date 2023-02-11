a37053b90ab5ce8a4c5065fc0395e6204729cff1
mar <rojanomariano@gmail.com>
2023-02-11 07:32:17 -0800

    initilized middleware

--- Develop/server.js
+++ Develop/server.js
@@ -4,3 +4,10 @@ const path = require("path");
 // requiring the index.js file 
 const api = require("./routes/index.js")
 
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true }));
+app.use("api", api);
