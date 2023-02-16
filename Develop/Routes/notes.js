46388e380ce3d1bc17809ea1be843e9e9c702caf
mar <rojanomariano@gmail.com>
2023-02-16 03:16:39 -0800

    post route to add notes

--- Develop/Routes/notes.js
+++ Develop/Routes/notes.js
@@ -5,4 +5,32 @@ const readFileAsync = util.promisify(fs.readFile);
 const uuid = require("uniqid");
 const {response} = require(".");
 
+// API get request 
+notes.get("/", (req, res) => {
+    console.log("here"); 
+    console.info(`${req.method} request to get notes received`);
+    readFileAsync(`db/sb.json`, "utf8").then((data) => {
+        let noteArr = JSON.parse(data);
+        console.log(noteArr);
+        res.json(noteArr);
+    });
+});
 
+// Post request for API
+notes.post("/", (req, res) => {
+    console.log("received");
+    console.info(`${req.method} request to add notes received`);
+    console.log(req.body);
+    
+    const {title, text} = req.body;
+    
+    if (title && text) {
+        console.log("if statement");
+         const newNote = {
+            title,
+            text,
+            id: uuid(),
+         };
+         console.log(newNote);
+    }
+})
